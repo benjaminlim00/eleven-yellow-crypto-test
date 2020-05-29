454f379b82c58cdfd626c9519741c0868d66fb37
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-29 22:35:00 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <main className="bg-background w-screen h-screen">
+      <h1>new react app</h1>
+    </main>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        background: "#FAF6F6"
+      },
+      fontFamily: {
+        // montserrat: ["montserrat", "sans-serif"]
+        display: ["montserrat", "sans-serif"],
+        body: ["montserrat", "sans-serif"]
+      }
+      // textColor: {
+      //   primary: "#C2948A"
+      // }
+    }
+  },
+  variants: {},
+  plugins: []
+};
