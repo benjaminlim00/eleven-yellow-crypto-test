081d5c9acdc58957403723f72933dbd68ec01c4f
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-30 20:48:53 +0800

    chore: update all js files to tsx

--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const container = "container mx-auto px-8 md:px-16";
-export const api = "https://compare.monedero.com/api/getPrice";
---
+++ src/constants.tsx
@@ -0,0 +1,2 @@
+export const container: string = "container mx-auto px-8 md:px-16";
+export const api: string = "https://compare.monedero.com/api/getPrice";
