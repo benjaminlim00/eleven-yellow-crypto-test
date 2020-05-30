2af07a91f3d35cbd2367f954cb77ada0283d0204
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-30 19:14:02 +0800

    chore: update currency options pairs

--- src/components/CurrencyOptions.tsx
+++ src/components/CurrencyOptions.tsx
@@ -1,9 +1,10 @@
 export const currencyOptions: string[] = [
   "BTC-LTC",
   "BTC-ETH",
+  "BTC-USDT",
   "ETH-LTC",
-  "ETH-USD",
-  "ETH-BTC"
+  "ETH-BTC",
+  "ETH-USDT"
 ];
 
 //to populate with more options. I do not have access to all the available currency pairs
