68ddc7c5f1a4fa239ded39e14a8139b25300a71e
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-30 19:07:43 +0800

    use https api call

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const container = "container mx-auto px-8 md:px-16";
-export const api = "http://compare.monedero.com/api/getPrice";
+export const api = "https://compare.monedero.com/api/getPrice";
